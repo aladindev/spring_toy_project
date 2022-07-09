7334655edffd669293d7faf16cfb6018d66a5665
aladindev <kjune0205@naver.com>
2022-07-10 02:22:13 +0900

    login form

---
+++ src/main/java/com/aladin/project/IndexController.java
@@ -0,0 +1,16 @@
+package com.aladin.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView("login");
+
+        return mv;
+    }
+}
