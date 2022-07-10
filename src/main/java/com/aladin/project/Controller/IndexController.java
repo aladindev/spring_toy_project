4906bf912ddd985bce72aae83dad7a6dc27d8b29
aladindev <kjune0205@naver.com>
2022-07-10 23:22:39 +0900

    2022.07.11 패스워드 암호화

--- src/main/java/com/aladin/project/Controller/IndexController.java
+++ src/main/java/com/aladin/project/Controller/IndexController.java
@@ -2,6 +2,7 @@ package com.aladin.project.Controller;
 
 import com.aladin.project.CommonUtils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class IndexController {
 
-    @Autowired(required = false)
-    CommonUtils commonUtils;
+    @Value("${aes.key}")
+    private String key; /*복호화 키 값*/
+    @Value("${aes.iv}")
+    private String iv; /*복호화 벡터 값*/
+    @Value("${aes.alg}")
+    private String alg; /*복호화 방식*/
 
     @GetMapping("/")
     public ModelAndView login() {
@@ -30,11 +35,14 @@ public class IndexController {
 
     @PostMapping("/join") /* 회원가입 */
     public ModelAndView userInfoInsert(HttpServletRequest request) throws Exception {
+        CommonUtils commonUtils = new CommonUtils(); //공통모듈
         String id = request.getParameter("id");
         String pwd = request.getParameter("password");
-        System.out.println(id + " / " + pwd);
-        String encPwd = commonUtils.encrypt(pwd);
-        System.out.println(encPwd);
+
+        //패스워드 암호화
+        String encPwd = commonUtils.encrypt(key, iv, alg, pwd);
+
+        System.out.println("test ::: " + encPwd);
         return null;
     }
 }
