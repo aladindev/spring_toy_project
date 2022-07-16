a3224585d3a63b0810b7e8fae0ec2768a08699c1
aladindev <kjune0205@naver.com>
2022-07-16 18:51:28 +0900

    JSON casting 로직 추가

--- src/main/java/com/aladin/project/Controller/CoinListController.java
+++ src/main/java/com/aladin/project/Controller/CoinListController.java
@@ -3,24 +3,29 @@ package com.aladin.project.Controller;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.internal.http.HttpHeaders;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import javax.swing.text.html.parser.Entity;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.UUID;
 
 @RestController
 @Slf4j
-public class CoinAutoTradeController {
+public class CoinListController {
 
     @Value("${upbit.a.key}")
     private String aKey;
@@ -43,20 +48,49 @@ public class CoinAutoTradeController {
         String authenticationToken = "Bearer " + jwtToken;
 
         try {
+            //Http 통신 객체 생성
             HttpClient client = HttpClientBuilder.create().build();
             HttpGet request = new HttpGet(acntListUrl);
 
+            //요청 헤더 세팅
             request.setHeader("Content-Type", "application/json");
             request.addHeader("Authorization", authenticationToken);
 
+            //응답값 가공
             HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
+            StringBuilder content = new StringBuilder();
 
-            log.info("entity to String : " + EntityUtils.toString(entity, "UTF-8"));
+            // inputStream으로 읽어들인다.
+            String line;
+            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+            //버퍼 라인으로 읽어들인다.
+            while(null != (line = br.readLine())) {
+                content.append(line);
+            }
+            //JSONArray로 캐스팅
+            Object obj = JSONValue.parse(content.toString());
+            JSONArray jsonData = (JSONArray)obj;
+
+            log.info("jsonData : " + jsonData);
 
         } catch (IOException e) {
             log.error("" + e.getStackTrace());
         }
     }
 
+    @GetMapping("minChartList.do")
+    public String minChartList() throws IOException {
+
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet("https://api.upbit.com/v1/candles/minutes/1?market=KRW-GRS&count=1");
+
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            String result = EntityUtils.toString(entity, "UTF-8");
+            log.info("result ::: " + result);
+
+        return result;
+    }
 }
