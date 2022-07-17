e56d6e28ef7688cd937685d916b3a53dbf4bc2fd
aladindev <kjune0205@naver.com>
2022-07-17 23:51:36 +0900

    Http 통신 공통화

--- src/main/java/com/aladin/project/CommonUtils/HttpConnectModule.java
+++ src/main/java/com/aladin/project/CommonUtils/HttpConnectModule.java
@@ -1,8 +1,17 @@
 package com.aladin.project.CommonUtils;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *  Http 통신 공통화
  * */
 
 public class HttpConnectModule {
+
+
+    private List<Map<String, Object>> httpConnect() {
+
+        
+    }
 }
