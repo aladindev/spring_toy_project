5589771abcb2cbcbd3d7cdacefba1648d4f238cc
aladindev <kjune0205@naver.com>
2022-07-30 15:53:54 +0900

    Http connection 공통화

--- src/main/java/com/aladin/project/CommonUtils/HttpConnectModule.java
+++ src/main/java/com/aladin/project/CommonUtils/HttpConnectModule.java
@@ -1,5 +1,14 @@
 package com.aladin.project.CommonUtils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +16,21 @@ import java.util.Map;
  *  Http 통신 공통화
  * */
 
+@Slf4j
 public class HttpConnectModule {
 
+    public String getEntity(String reqUrl) throws IOException {
 
-    /*public List<Map<String, Object>> httpConnect(String k, String s) {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(reqUrl);
+        //oracle 자율 트랜잭션
+        HttpResponse response = client.execute(request);
 
+        HttpEntity entity = response.getEntity();
 
-
+        String result = EntityUtils.toString(entity, "UTF-8");
+        log.info("result ::: " + result);
 
         return null;
-    }*/
+    }
 }
